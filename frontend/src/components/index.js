839d6acb37f95f29a0a6112ae5e5db6acb3bce9f
Kamran Ayaz <224069719+fastwithkamran@users.noreply.github.com>
2026-07-09 12:50:29 +0500

    Create index.js for ease of imports/exports components

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,4 +1,28 @@
-import Signup from "./Signup"
-import Login from "./Login"
+import Signup from "./Signup";
+import Login from "./Login";
 
-export {Signup, Login}
+import Navbar from "./header/Navbar";
+
+import Home from './Home.jsx'
+
+import SellerHome from "./hero/SellerHome.jsx";
+import BuyerHome from "./hero/BuyerHome.jsx";
+
+import CreateProperty from "./property/CreateProperty";
+import PropertyPage from "./property/PropertyPage";
+import SellerPosts from "./property/SellerPosts";
+
+import Footer from "./footer/Footer.jsx"
+
+export {
+  Signup,
+  Login,
+  Navbar,
+  Home,
+  SellerHome,
+  BuyerHome,
+  CreateProperty,
+  PropertyPage,
+  SellerPosts,
+  Footer,
+};
