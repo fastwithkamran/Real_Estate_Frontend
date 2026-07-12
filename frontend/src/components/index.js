4d17f26e8038f666871b5ac20a5d2cf24bee773e
Kamran Ayaz <224069719+fastwithkamran@users.noreply.github.com>
2026-07-12 17:13:54 +0500

    Added routes for Settings and AllPosts for a particular request

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -8,6 +8,9 @@ import Home from "./Home.jsx";
 import CreateProperty from "./property/CreateProperty";
 import PropertyPage from "./property/PropertyPage";
 
+import Settings from "./user/Settings.jsx";
+import AllPosts from "./user/AllPosts.jsx";
+
 import Footer from "./footer/Footer.jsx";
 
 export {
@@ -17,5 +20,7 @@ export {
   Home,
   CreateProperty,
   PropertyPage,
+  Settings,
+  AllPosts,
   Footer,
 };
