86ffc6438baa791621daa293903428b7beb9010c
Kamran Ayaz <224069719+fastwithkamran@users.noreply.github.com>
2026-07-12 17:13:26 +0500

    Optimized the UI and add Toaster functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,30 @@
 import { Navbar } from "./components";
 import { Footer } from "./components";
 import { Outlet, useLocation } from "react-router";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   const location = useLocation();
   const atHome = location.pathname === "/";
+  const atPage = location.pathname === "/user/property-page";
 
   return (
     <>
-      <div className="flex flex-col min-h-screen w-full overflow-x-hidden">
-        <nav>
+      <div>
+        <Toaster position="top-center" />
+      </div>
+      <div className="flex flex-col min-h-dvh w-full overflow-x-hidden bg-gray-200">
+        <nav className="mb-3">
           <Navbar />
         </nav>
         <main
           className={
-            atHome
-              ? "flex flex-col justify-center items-center bg-gray-200 p-4"
-              : "flex flex-col grow justify-center items-center bg-gray-200 p-4"
+           `flex flex-col grow ${!atHome && !atPage} ?  "justify-center items-center p-auto" : ""`
           }
         >
           <Outlet />
         </main>
-        <footer>
+        <footer className="mt-3">
           <Footer />
         </footer>
       </div>
