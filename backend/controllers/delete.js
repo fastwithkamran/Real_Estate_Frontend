95d7d5a8ed4bf28635f742d992951de2a1bcf0aa
Kamran Ayaz <224069719+fastwithkamran@users.noreply.github.com>
2026-07-13 13:40:28 +0500

    Create the delete user account functionality

---
+++ backend/controllers/delete.js
@@ -0,0 +1,22 @@
+const User = require("../models/user");
+const Property = require("../models/property");
+
+const handleUserDelete = async (req, res) => {
+  try {
+    const propertiesDelete = await Property.deleteMany({
+      createdBy: req.params.id,
+    });
+
+    const userDelete = await User.findByIdAndDelete(req.params.id);
+    if (userDelete) {
+      return res.status(200).json({ msg: "Account Deleted Successfully" });
+    } else {
+      return res.status(404).json({ msg: "User Not Found" });
+    }
+  } catch (error) {
+    console.error("Error Deleting User Account ", error);
+    return res.status(500).json({ msg: "Account Cannot Deleted" });
+  }
+};
+
+module.exports = handleUserDelete;
