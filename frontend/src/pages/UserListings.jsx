1f14077158531b75d5e217d46323b8066f89162f
Kamran Ayaz <224069719+fastwithkamran@users.noreply.github.com>
2026-07-19 15:13:13 +0500

    Add icons in the card in UserListing page

--- frontend/src/pages/UserListings.jsx
+++ frontend/src/pages/UserListings.jsx
@@ -3,6 +3,7 @@ import { useNavigate, Link } from "react-router";
 import { toast } from "react-hot-toast";
 import { useSelector } from "react-redux";
 import { FaLocationDot } from "react-icons/fa6";
+import { FaBed, FaBath, FaCouch } from "react-icons/fa";
 
 function UserListings() {
   const [properties, setProperties] = useState([]);
@@ -37,7 +38,7 @@ function UserListings() {
     const fetchData = async () => {
       try {
         const response = await fetch(
-          `/api/property/allProperties/${currentUser._id}`,
+          `/api/property/userProperties/${currentUser._id}`,
           {
             method: "GET",
           },
@@ -108,34 +109,43 @@ function UserListings() {
               <h3 className="text-lg font-medium text-center mt-2">
                 {property.title}
               </h3>
-              <p className="text-gray-700 flex items-center">
+              <p className="text-gray-700 flex items-center justify-center">
                 <FaLocationDot className="text-red-600" />
                 {property.city}, {property.province}
               </p>
-              <p className="text-sm text-gray-700 mt-2 text-start">
+              <p className="text-medium text-gray-700 mt-2 text-start">
                 Type: {property.type}{" "}
               </p>
               <p className="text-medium text-slate-500 mt-4 text-start">
                 {property.price}
               </p>
-              <div className="flex gap-6 text-sm text-black mt-2">
-              <p>{property.bedrooms} beds</p>
-              <p>{property.bathrooms} bathrooms</p>
+              <div className="flex gap-6 text-sm text-green-600 mt-2">
+                <p className="flex items-center gap-2">
+                  <FaBed />
+                  {property.bedrooms} beds
+                </p>
+                <p className="flex items-center gap-2">
+                  <FaBath />
+                  {property.bathrooms} bathrooms
+                </p>
+                <div className="flex items-center gap-2">
+                  <FaCouch /> {property.furnish ? "Furnished" : "UnFurnished"}
+                </div>
               </div>
               <div className="flex flex-col">
-              <Link
-                to={`/listing-page/${property._id}`}
-                className="mt-2 bg-blue-500 text-white text-center py-2 px-4 rounded hover:bg-blue-600"
-              >
-                View Details
-              </Link>
-              <button
-                onClick={() => handlePropertyDelete(property._id)}
-                className="mt-2 bg-red-500 text-white py-2 px-4 rounded hover:bg-red-600"
-              >
-                Delete
-              </button>
-            </div>
+                <Link
+                  to={`/listing-page/${property._id}`}
+                  className="mt-2 bg-blue-500 text-white text-center py-2 px-4 rounded hover:bg-blue-600"
+                >
+                  View Details
+                </Link>
+                <button
+                  onClick={() => handlePropertyDelete(property._id)}
+                  className="mt-2 bg-red-500 text-white py-2 px-4 rounded hover:bg-red-600"
+                >
+                  Delete
+                </button>
+              </div>
             </div>
           ))}
         </div>
